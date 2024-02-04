867dbfd176a7175b49d1fba0941187787996fa97
Phaylali <admin@omniversify.com>
2024-02-04 01:55:38 +0100

    removing mdx due to dependency conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +9,6 @@ export default defineConfig({
     viewTransitions: true,
   },
   site: 'https://gaming.omniversify.com',
-  //site:'http://localhost:3000/',
-  integrations: [tailwind(), compress(), react(), mdx()],
+  
+  integrations: [tailwind(), compress(), react(),],
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,5 @@
 import rss from "@astrojs/rss";
-import { getCollection, CollectionEntry } from "astro:content";
+import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 export async function get(context) {
   const posts = await getCollection('blog');
