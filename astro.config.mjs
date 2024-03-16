fd858131e386c3da24ed03a23c15e6fa202436f0
Phaylali <admin@omniversify.com>
2024-03-16 21:09:28 +0000

    adding vercel analytics and speed iinsights

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
+  output: 'static',
+  adapter: vercel(),
   site: 'https://gaming.omniversify.com',
   
   integrations: [tailwind(), compress(), react(),],
