b446f1e9d811643e5fdadc9d50e0bf69ba8c50ec
Phaylali <admin@omniversify.com>
2024-03-16 19:32:16 +0000

    fixing rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,7 +14,7 @@ export async function get(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `/blog/${post.slug}/`,
     })),
   });
 }
