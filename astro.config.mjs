70e14ebda786b048edb2eac00d6f2f18a509dc66
Phaylali <admin@omniversify.com>
2024-03-17 05:02:29 +0000

    ttesting without vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
+//import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+ // output: 'static',
+ // adapter: vercel({
+ //   webAnalytics: { enabled: true }
+ // }),
   site: 'https://gaming.omniversify.com',
   
   integrations: [tailwind(), compress(), react(),],
