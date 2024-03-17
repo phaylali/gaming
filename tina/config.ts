c5b8ae47ad038dac6aac122a08fa874a32966a38
Phaylali <admin@omniversify.com>
2024-03-17 03:56:29 +0000

    test

--- tina/config.ts
+++ tina/config.ts
@@ -1,65 +1,67 @@
-import { defineConfig } from "tinacms";
-
+import { UsernamePasswordAuthJSProvider, TinaUserCollection } from "tinacms-authjs/dist/tinacms";
+import { defineConfig, LocalAuthProvider } from "tinacms";
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
-
+const branch = process.env.GITHUB_BRANCH ||
+    process.env.VERCEL_GIT_COMMIT_REF ||
+    process.env.HEAD ||
+    "main";
 export default defineConfig({
-  branch:"main",
-
-  // Get this from tina.io
-  clientId: process.env.TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
-
-  build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "public",
+    contentApiUrlOverride: "/api/tina/gql",
+    authProvider: isLocal ? new LocalAuthProvider() : new 
+    // Your hosting provider likely exposes this as an environment variable
+    UsernamePasswordAuthJSProvider(),
+    branch: "main",
+    // Get this from tina.io
+    clientId: process.env.TINA_CLIENT_ID,
+    // Get this from tina.io
+    token: process.env.TINA_TOKEN,
+    build: {
+        outputFolder: "admin",
+        publicFolder: "public",
     },
-  },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "src/content/blog",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "string",
-            name: "description",
-            label: "Description",
-            required: true,
-          },
-          {
-            type: "datetime",
-            name: "pubDate",
-            label: "Date",
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+    media: {
+        tina: {
+            mediaRoot: "",
+            publicFolder: "public",
+        },
+    },
+    // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+    schema: {
+        collections: [
+            TinaUserCollection,
+            {
+                name: "post",
+                label: "Posts",
+                path: "src/content/blog",
+                fields: [
+                    {
+                        type: "string",
+                        name: "title",
+                        label: "Title",
+                        isTitle: true,
+                        required: true,
+                    },
+                    {
+                        type: "string",
+                        name: "description",
+                        label: "Description",
+                        required: true,
+                    },
+                    {
+                        type: "datetime",
+                        name: "pubDate",
+                        label: "Date",
+                        required: true,
+                    },
+                    {
+                        type: "rich-text",
+                        name: "body",
+                        label: "Body",
+                        isBody: true,
+                    },
+                ],
+            }
+        ]
+    }
 });
