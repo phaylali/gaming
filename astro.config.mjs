cfd94ac1f7ecb2d382930202b624a94f4878d076
Phaylali <admin@omniversify.com>
2024-04-08 06:17:19 +0000

    adding social sharing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import react from '@astrojs/react';
-
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   
   
   site: 'https://gaming.omniversify.com',
-  integrations: [tailwind(), compress(), react(),]
+  integrations: [tailwind(), compress(), react()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
 });
